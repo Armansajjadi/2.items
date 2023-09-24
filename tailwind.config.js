31c43b5507d88ea38d78d3e52519b7154c0ffb50
Arman <Arman.sajadi10@gmail.com>
2023-09-24 15:24:32 +0330

    hi

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height:{
+        100:"550px"
+      }
+    },
+  },
+  plugins: [],
+}
+
